0489fb79f6736572cd21d20e6a826b034f2b56c4
niharikavaddi <36045615+niharikavaddi@users.noreply.github.com>
2019-06-13 16:32:05 +0530

    vowelconsonant.cpp

---
+++ vowelconsonant.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cin>>ch;
+	if((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+	{
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	{
+		cout<<"Vowel";
+	}
+	else
+	{
+		cout<<"Consonant";
+	}
+    }
+    else
+    {
+    	cout<<"invalid";
+	}
+	return 0;
+}
