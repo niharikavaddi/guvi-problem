ff4fea96fdd8c3cad701afa812bd5934c7bd35a1
niharikavaddi <36045615+niharikavaddi@users.noreply.github.com>
2019-06-13 18:24:21 +0530

    oddnum.cpp

---
+++ oddnum.cpp
@@ -0,0 +1,16 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	unsigned int limit1,limit2,i;
+ 	cin>>limit1>>limit2;
+ 	for(i=limit1+1;i<limit2;i++)
+ 	{
+ 		if(i%2!=0)
+ 		{
+ 		cout<<i<<" ";	
+		}
+	}
+
+ 	return 0;
+ }
